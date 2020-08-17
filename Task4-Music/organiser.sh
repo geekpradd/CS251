a0dbadaac3804b2935a320eb65877927b65b4288
Harshit Gupta <59705530+HARSHIT-GUPTA-coder@users.noreply.github.com>
2020-08-17 19:16:50 +0530

    Added Task 1,4 and 5

---
+++ Task4-Music/organiser.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+mkdir -p albums
+mkdir -p playlists
+
+for f in songs/*
+do
+    IFS=$'\n' line=($(<$f))
+    mkdir -p "albums/${line[0]}"
+    ln -sf $f "albums/${line[0]}/$(basename $f)"
+    for i in "${line[@]:2}"
+    do
+        mkdir -p "playlists/$i"
+        ln -sf $f "playlists/$i/$(basename $f)"
+    done
+done
