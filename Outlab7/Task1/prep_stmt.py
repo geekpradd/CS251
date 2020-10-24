99016780404958e2e5169c22d2cd0bca2e535930
Harshit Gupta <mail4harshit1@gmail.com>
2020-10-24 22:02:26 +0530

    corrected q7

--- Outlab7/Task1/prep_stmt.py
+++ Outlab7/Task1/prep_stmt.py
@@ -1,18 +1,22 @@
 import sqlite3, sys
 
 c = sqlite3.connect('ipl.db')
+c.set_trace_callback(print)
 
 case = int(sys.argv[1]) - 1
-
 names = ["TEAM", "PLAYER", "MATCH", "PLAYER_MATCH", "BALL_BY_BALL"]
 
-total = [2, 6, 15, 7, 11]
+def get_col(table):
+    cur = c.execute("Select * from {}".format(table))
+    nam = list(map(lambda x: x[0],cur.description))
+    return len(nam)
 
+def get_query(table): 
+    return "INSERT INTO {0} VALUES({1}?);".format(table, '?,'*(get_col(table)-1))
 
-data = "'" + "', '".join(sys.argv[2:2+total[case]]) + "'"
-
-query = "INSERT INTO {0} VALUES({1});".format(names[case], data)
-c.execute(query)
+query = get_query(names[case])
+print(query)
+c.execute(query,tuple(sys.argv[2:]))
 
 c.commit()
 c.close()
